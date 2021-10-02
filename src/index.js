91769d028f492b2ec77980c97b54c90b9d44bbfc
rryaboshtan <rrvworkroman@gmail.com>
2021-10-02 22:21:24 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './assets/css/grid.css';
+import './assets/css/index.css';
+
+import Layout from './components/layout/layout';
+
+document.title = 'My dashboard';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <Layout />
+   </React.StrictMode>,
+   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
