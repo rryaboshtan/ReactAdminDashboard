6d097b440fa599ae42f0405b02703186d1bcef78
rryaboshtan <rrvworkroman@gmail.com>
2021-10-03 02:31:03 +0300

    styling sidebar menu

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,29 +1,42 @@
-import React from 'react'
+import React from 'react';
 
-// import './sidebar.css'
+import './sidebar.css'
 
-import logo from '../../assets/images/logo.png'
+import logo from '../../assets/images/logo.png';
 
-import sidebar_items from '../../assets/JsonData/sidebar_routes.json'
-import { Link } from 'react-router-dom'
+import sidebar_items from '../../assets/JsonData/sidebar_routes.json';
+import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
-    return (
-        <div className = 'sidebar'>
-            <div className="sidebar__logo">
-                <img src={logo} alt="company logo" />
-            </div>
-            {
-                sidebar_items.map((item, index) => (
-                    <Link to={item.route} key={index}>
-                        <div>
-                            {item.display_name}
-                        </div>
-                    </Link>
-                ))
-            }
-        </div>
-    )
-}
+const SidebarItem = (props) => {
+   const active = props.active ? 'active' : '';
 
-export default Sidebar
+   return (
+      <div className="sidebar__item">
+         <div className={`sidebar__item-inner ${active}`}>
+            <i className={props.icon}></i>
+            <span>{props.title}</span>
+         </div>
+      </div>
+   );
+};
+
+const Sidebar = (props) => {
+
+   const activeItem = sidebar_items.findIndex((item) => item.route === props.location.pathname);
+
+   return (
+      <div className="sidebar">
+         <div className="sidebar__logo">
+            <img src={logo} alt="company logo" />
+         </div>
+         {sidebar_items.map((item, index) => (
+            <Link to={item.route} key={index}>
+               {/* <div>{item.display_name}</div> */}
+               <SidebarItem title={item.display_name} item={item.icon} active={index === activeItem}></SidebarItem>
+            </Link>
+         ))}
+      </div>
+   );
+};
+
+export default Sidebar;
