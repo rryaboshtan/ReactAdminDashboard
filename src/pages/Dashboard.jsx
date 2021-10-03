bd30b537ac2b7a29ee87e31b155f7d7721d72663
rryaboshtan <rrvworkroman@gmail.com>
2021-10-03 21:48:40 +0300

    order table complete

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,35 +8,38 @@ import StatusCard from '../components/statusCard/StatusCard';
 import statusCards from '../assets/JsonData/status-card-data.json';
 
 const chartOptions = {
-   series: [{
-      name: 'Online Customers',
-      data: [40, 70, 20, 90, 36, 80, 30, 91, 60]
-   }, {
-      name: 'Store Customers',
-      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
-      }],
+   series: [
+      {
+         name: 'Online Customers',
+         data: [40, 70, 20, 90, 36, 80, 30, 91, 60],
+      },
+      {
+         name: 'Store Customers',
+         data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10],
+      },
+   ],
    options: {
       color: ['#6ab04c', '#2980b9'],
       chart: {
-         background: 'transparent'
+         background: 'transparent',
       },
       dataLabels: {
-         enabled: false
+         enabled: false,
       },
       stroke: {
-         curve: 'smooth'
+         curve: 'smooth',
       },
       xaxis: {
-         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
       },
       legend: {
-         position: 'top'
+         position: 'top',
       },
       grid: {
-         show: false
-      }
-   }
-}
+         show: false,
+      },
+   },
+};
 
 const topCustomers = {
    head: ['user', 'total orders', 'total spending'],
@@ -69,11 +72,73 @@ const topCustomers = {
    ],
 };
 
-const renderCustomerHead = (item) => (
+const latestOrders = {
+   head: ['order_id', 'user', 'total price', 'date', 'status'],
+   body: [
+      {
+         id: '#0d1711',
+         user: 'john doe',
+         date: '17 Jun 2021',
+         price: '$900',
+         status: 'shipping',
+      },
+      {
+         id: '#0d1712',
+         user: 'frank iva',
+         date: '1 Jun 2021',
+         price: '$400',
+         status: 'paid',
+      },
+      {
+         id: '#0d1713',
+         user: 'anthony baker',
+         date: '27 Jun 2021',
+         price: '$200',
+         status: 'pending',
+      },
+      {
+         id: '#0d1714',
+         user: 'frank doe',
+         date: '3 Jun 2021',
+         price: '$400',
+         status: 'paid',
+      },
+      {
+         id: '#0d1715',
+         user: 'anthony lasker',
+         date: '23 Jun 2021',
+         price: '$240',
+         status: 'refund',
+      },
+   ],
+};
+
+const orderStatus = {
+   shipping: 'primary',
+   pending: 'warning',
+   paid: 'success',
+   refund: 'danger',
+};
+
+const renderOrderHead = item => (
    <th key={uuidv4()}>
       {item}
-   </th>
+   </th> 
 )
+
+const renderOrderBody = (item) => (
+   <tr>
+      <td>{item.id}</td>
+      <td>{item.user}</td>
+      <td>{item.price}</td>
+      <td>{item.date}</td>
+      <td>
+         <span>{item.status}</span>
+      </td>
+   </tr>
+);
+
+const renderCustomerHead = (item) => <th key={uuidv4()}>{item}</th>;
 
 const renderCustomerBody = (item) => (
    <tr key={uuidv4()}>
@@ -103,6 +168,8 @@ const Dashboard = () => {
                   <Chart options={chartOptions.options} series={chartOptions.series} type='line' height='100%' />
                </div>
             </div>
+         </div>
+         <div className='row'>
             <div className='col-4'>
                <div className='card'>
                   <div className='card_header'>
@@ -114,11 +181,28 @@ const Dashboard = () => {
                         renderHead={renderCustomerHead}
                         bodyData={topCustomers.body}
                         renderBody={renderCustomerBody}
-                     >
-                     </Table>
+                     ></Table>
                   </div>
                   <div className='card__footer'>
                      <Link to='/'>View all</Link>
+                  </div>
+               </div>
+            </div>
+            <div className='col-8'>
+               <div className='card'>
+                  <div className='card__header'>
+                     <h3>latest orders</h3>
+                  </div>
+                  <div className='card__body'>
+                     <Table
+                        headData={latestOrders.head}
+                        renderHead={renderOrderHead}
+                        bodyData={latestOrders.body}
+                        renderBody={renderOrderBody}
+                     ></Table>
+                  </div>
+                  <div className='card__footer'>
+                     <Link to='/'>view all</Link>
                   </div>
                </div>
             </div>
