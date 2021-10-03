e234ee324eaeef9aa4c395f56b8637e21ce1d0c9
rryaboshtan <rrvworkroman@gmail.com>
2021-10-03 17:38:15 +0300

    add chart options

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import Chart from 'react-apexcharts';
 
 import StatusCard from '../components/statusCard/StatusCard';
 import statusCards from '../assets/JsonData/status-card-data.json';
+
+const chartOptions = {
+   series: [{
+      name: 'Online Customers',
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60]
+   }, {
+      name: 'Store Customers',
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
+      }],
+   options: {
+      color: ['#6ab04c', '#2980b9'],
+      chart: {
+         background: 'transparent'
+      },
+      dataLabels: {
+         enabled: false
+      },
+      stroke: {
+         curve: 'smooth'
+      },
+      xaxis: {
+         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+      },
+      legend: {
+         position: 'top'
+      },
+      grid: {
+         show: false
+      }
+   }
+}
 
 const Dashboard = () => {
    return (
@@ -10,21 +42,17 @@ const Dashboard = () => {
          <div className='row'>
             <div className='col-6'>
                <div className='row'>
-                  { statusCards.map((item) => (
+                  {statusCards.map((item) => (
                      <div className='col-6'>
                         {item.title}
-                        <StatusCard
-                           icon={item.icon}
-                           count={item.count}
-                           title={item.title}
-                        />
+                        <StatusCard icon={item.icon} count={item.count} title={item.title} />
                      </div>
                   ))}
                </div>
             </div>
-            <div className="col-6">
-               <div className="card full-height">
-                  
+            <div className='col-6'>
+               <div className='card full-height'>
+
                </div>
             </div>
          </div>
