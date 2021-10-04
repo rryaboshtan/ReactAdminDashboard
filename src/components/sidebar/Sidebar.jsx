1f69412301c97bc1f6b040abb50e834d127c1eb3
rryaboshtan <rrvworkroman@gmail.com>
2021-10-05 02:21:01 +0300

    change logo

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './sidebar.css';
 
-import logo from '../../assets/images/logo.png';
+import logo from '../../assets/images/favicon.png';
 
 import sidebar_items from '../../assets/JsonData/sidebar_routes.json';
 import { Link } from 'react-router-dom';
